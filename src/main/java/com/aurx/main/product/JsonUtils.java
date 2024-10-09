d1d263037f9132dd20028ff7e6fe6299a42ae04a
Deepak Purohit <deepak.purohit@aurx.co>
2024-10-09 16:51:40 +0530

    updated main class and change fetched functionality by Added Product bean

--- src/main/java/com/aurx/main/product/JsonUtils.java
+++ src/main/java/com/aurx/main/product/JsonUtils.java
@@ -5,7 +5,18 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class JsonUtils {
+    private static final String ID="id";
+    private static final String TITLE="title";
+    private static final String DESCRIPTION="description";
+    private static final String PRICE="price";
+    private static final String BRAND = "brand";
+    private static final String IMAGES="images";
     public static JsonArray parsingJson(String strJson){
         try {
             JsonObject jsonObject = JsonParser.parseString(strJson).getAsJsonObject();
@@ -16,36 +27,51 @@ public class JsonUtils {
         }
     }
 
-    public static void fetchingData(int data, JsonArray jsonArray) {
+    public static List<Product> fetchingProduct(int data, JsonArray jsonArray) {
         if (data > jsonArray.size()) {
             System.out.println(data + " is insufficient; only " + jsonArray.size() + " products available.");
-            return;
+            return null;
         }
+        List<Product> products=new ArrayList<>();
             for (int i = 0; i < data; i++) {
                 JsonObject jsonProduct = jsonArray.get(i).getAsJsonObject();
-                System.out.println("Product " + (i + 1));
-
-                int id = jsonProduct.get("id").getAsInt();
-                System.out.println("id " + id);
-                String title = jsonProduct.get("title").getAsString();
-                System.out.println("title " + title);
-                String description = jsonProduct.get("description").getAsString();
-                System.out.println("description " + description);
-                double price = jsonProduct.get("price").getAsDouble();
-                System.out.println("price " + price);
-                if (jsonProduct.has("brand") && !jsonProduct.get("brand").isJsonNull()) {
-                    String brand = jsonProduct.get("brand").getAsString();
-                    System.out.println("Brand: " + brand);
+                int id = jsonProduct.get(ID).getAsInt();
+                String title = jsonProduct.get(TITLE).getAsString();
+                String description = jsonProduct.get(DESCRIPTION).getAsString();
+                double price = jsonProduct.get(PRICE).getAsDouble();
+                String brand="";
+                if (jsonProduct.has(BRAND) && !jsonProduct.get(BRAND).isJsonNull()) {
+                     brand = jsonProduct.get(BRAND).getAsString();
                 }
-
-                JsonArray images = jsonProduct.getAsJsonArray("images");
+                JsonArray images = jsonProduct.getAsJsonArray(IMAGES);
+                String image = "";
                 if (images.size() > 0) {
-                    System.out.println("First Image URL: " + images.get(0).getAsString());
+                   image=images.get(0).getAsString();
                 } else {
                     System.out.println("No images available.");
                 }
-                System.out.println();
+                products.add(new Product(id,title,description,price,brand,image));
             }
+            return products;
         }
+    public static String getJsonFromUrl(String theUrl) {
+        if (theUrl == null && theUrl.isEmpty()) {
+            return null;
+        }
+        StringBuilder content = new StringBuilder();
+        try {
+            URL url = new URL(theUrl);
+            Scanner scanner = new Scanner(url.openStream());
+            while (scanner.hasNext()) {
+                content.append(scanner.next()).append(" ");
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error fetching data from URL:" + e.getMessage());
+            ;
+        }
+
+        return content.toString();
+    }
     }
 
